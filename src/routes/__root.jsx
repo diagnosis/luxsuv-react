8a6cc0342a7e11ca04a57481707b4699ec858001
safa demirkan <demirkansafa@gmail.com>
2025-05-31 15:56:10 -0700

    Remove TanStack Router Devtools

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -1,5 +1,4 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
 
@@ -11,7 +10,6 @@ export const Route = createRootRoute({
                 <Outlet />
             </main>
             <Footer />
-            <TanStackRouterDevtools />
         </div>
     ),
 });
