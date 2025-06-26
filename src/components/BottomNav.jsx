0aa49119a93bb66c61ea801dd279ed6a23cd6340
safa demirkan <demirkansafa@gmail.com>
2025-06-26 00:37:27 -0700

    Increase icon and text size in bottom navigation

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -9,29 +9,29 @@ const BottomNav = () => {
                     to="/"
                     className="flex flex-col items-center text-light hover:text-yellow transition-colors py-1"
                 >
-                    <Home className="w-3 h-3" />
-                    <span className="text-[8px] mt-0.5">Home</span>
+                    <Home className="w-5 h-5" />
+                    <span className="text-xs mt-0.5">Home</span>
                 </Link>
                 <Link
                     to="/services"
                     className="flex flex-col items-center text-light hover:text-yellow transition-colors py-1"
                 >
-                    <Car className="w-3 h-3" />
-                    <span className="text-[8px] mt-0.5">Services</span>
+                    <Car className="w-5 h-5" />
+                    <span className="text-xs mt-0.5">Services</span>
                 </Link>
                 <Link
                     to="/about"
                     className="flex flex-col items-center text-light hover:text-yellow transition-colors py-1"
                 >
-                    <Info className="w-3 h-3" />
-                    <span className="text-[8px] mt-0.5">About</span>
+                    <Info className="w-5 h-5" />
+                    <span className="text-xs mt-0.5">About</span>
                 </Link>
                 <Link
                     to="/book"
                     className="flex flex-col items-center text-light hover:text-yellow transition-colors py-1"
                 >
-                    <CalendarCheck className="w-3 h-3" />
-                    <span className="text-[8px] mt-0.5">Book Now</span>
+                    <CalendarCheck className="w-5 h-5" />
+                    <span className="text-xs mt-0.5">Book Now</span>
                 </Link>
             </div>
         </nav>
