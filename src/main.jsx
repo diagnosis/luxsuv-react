7d31023cb817685b910cec45d663e1811105ac34
safa demirkan <demirkansafa@gmail.com>
2025-06-26 00:49:55 -0700

    Fix ReactQueryDevtools import error

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query/devtools';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { queryClient } from './lib/queryClient';
 import './App.css'; // Import CSS here to ensure it's included in the build
 
